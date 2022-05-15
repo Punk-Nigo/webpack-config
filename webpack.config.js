55cdbd4e25f4145bd72951917d50e01fb8106db9
Nigo <nigo@Kimo.com>
2022-05-15 23:56:57 +0800

    add react support

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,50 @@
-let mode = "development";
+const MiniCssExtractPLugin = require('mini-css-extract-plugin');
 
-if(process.env.NODE_ENV === "production")
-{
+let mode = "development";
+let target = "web";
+
+if (process.env.NODE_ENV === "production") {
     mode = "production";
+    target = "browserslist";
 }
 
 module.exports = {
     mode: mode,
+    target: target,
     module: {
-        rules:[
+        rules: [
             {
-                test: /\.js$/,
-                exclude:/node_modules/,
+                test: /\.(s[ac]|c)ss$/i,
+                use: [
+                    MiniCssExtractPLugin.loader,
+                    "css-loader",
+                    "postcss-loader",
+                    "sass-loader"
+                ]
+            },
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
                 use: {
                     loader: "babel-loader"
                 }
             }
         ]
     },
-    devtool: "source-map", // false 關閉壓縮   source-map
+    plugins: [
+        new MiniCssExtractPLugin()
+    ],
+    resolve: {
+        extensions: [
+            ".js",
+            ".jsx"
+        ]
+    },
+    devtool: "source-map",
     devServer: {
-        static: './',
+        static: "./",
         compress: true,
-        port:8080
+        port: 8080,
+        hot: true
     },
 };
